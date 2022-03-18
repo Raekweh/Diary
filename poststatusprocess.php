6ebb5307a472602a24f3e64a111af682038323b0
Raekweh <Raekweh009@gmail.com>
2022-03-18 19:45:03 +1300

    Pseudocode

---
+++ poststatusprocess.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Post status process</title>
+        <link rel = "stylesheet" href = "style.css">
+</head>
+<body>
+    <?php
+        
+    ?>
+</body>
+</html>
