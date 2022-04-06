6f452258bf20f25de7c92b209678391fb2c6532e
Raymond Li <raekweh@Raymonds-MacBook-Air.local>
2022-04-06 17:09:45 +1200

    displaying table

--- searchstatusprocess.php
+++ searchstatusprocess.php
@@ -26,6 +26,7 @@
                     $statusinput = $_GET["searchinput"];
                     $searchQuery = "SELECT * FROM $sql_table WHERE status = '%$statusinput%'";
                     $result = mysqli_query($searchQuery);
+                    $row = mysqli_fetch_row($result);
 
                     //starting a table to display the sql queries
                     echo "<table>";
@@ -37,7 +38,16 @@
                         displayingHref();
                     }
                     else{
-                        
+                        while($row)
+                        {
+                            echo "<tr><th>code: </th><td>{$row[0]}</td>";
+                            echo "<th>status: </th><td>{$row[1]</td>";
+                            echo "<th>date: </th><td>{$row[2]</td>";
+                            echo "<th>permssion: </th><td>{$row[3]</td>";
+                            echo "<th>share: </th><td>{$row[4]</td></tr>";
+                            $row = mysqli_fetch_row($result);
+                        }
+                        displayingHref();
                     }  
                 }
                 echo "</table>";
@@ -46,11 +56,14 @@
                 }
             }
 
+            //Closing the database
+            mysql_close($conn);
+
             //Displaying the links
             function displayingHref()
             {
-                echo '<p><a id = "searchhref" href="http://cyz8072.cmslamp14.aut.ac.nz/assign1/searchstatusform.html">Search status</a></p>'
-                echo '<p><a id = "homehref" href="http://cyz8072.cmslamp14.aut.ac.nz/assign1/index.html">Return to Home</a></p>'
+                echo '<p><a id = "searchhref" href="http://cyz8072.cmslamp14.aut.ac.nz/assign1/searchstatusform.html">Search status</a>
+                <a id = "homehref" href="http://cyz8072.cmslamp14.aut.ac.nz/assign1/index.html">Return to Home</a></p>';
             }
             ?>
         </div>
