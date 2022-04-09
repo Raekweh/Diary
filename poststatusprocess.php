e381eb9f769bde2d58c6b3202939a0f63d25662a
Raekweh <Raekweh009@gmail.com>
2022-04-10 09:50:08 +1200

    adding divs to both process

--- poststatusprocess.php
+++ poststatusprocess.php
@@ -7,192 +7,197 @@
 </head>
 
 <body>
-    <div class="bodyContainer">
-        <?php
-        require_once('./conf/sqlinfo.php');
+    <div class = "container">
+        <div class = "heading">
+            <h1>Status Posting System</h1>
+        </div>
+        <div class="bodyContainer">
+            <?php
+            require_once('./conf/sqlinfo.php');
 
-        //Checks if the sqli_connection is a true or false
-        $conn = @mysqli_connect(
-            $sql_host,
-            $sql_user,
-            $sql_pass,
-            $sql_db
-        );
+            //Checks if the sqli_connection is a true or false
+            $conn = @mysqli_connect(
+                $sql_host,
+                $sql_user,
+                $sql_pass,
+                $sql_db
+            );
 
-        if (!$conn) 
-        {
-            echo "<p>Database connection failure <p>";
-        } 
-        else
-         {
-            if (checkCode($_POST["statuscode"]) && checkStatus($_POST["status"]) && validDate($_POST["date"])) 
+            if (!$conn) 
             {
-                echo "Congrats";
-
-                //Storing values
-                $code = $_POST["statuscode"];
-                $status = $_POST["status"];
-                $date = $_POST["date"];
-                $share = $_POST["share"];
-                $permisisonInput = $_POST["permission"];
-                $permission = "";
-                //Storing permission variables into a single string
-                foreach ($permisisonInput as $value) 
+                echo "<p>Database connection failure <p>";
+            } 
+            else
+            {
+                if (checkCode($_POST["statuscode"]) && checkStatus($_POST["status"]) && validDate($_POST["date"])) 
                 {
-                    $permission .= $value . " ";
-                }
+                    echo "Congrats";
 
-                $checkCodeQuery = "SELECT * FROM $sql_tble WHERE code = '$code'";
-
-                //Checking if the code exist in the database 
-                if(checkDatabase($checkCodeQuery, $conn))
-                {
-                    //Sqli queries
-                    $tableExistence = "SELECT 1 FROM $sql_tble";
-                    $creatingTable = "CREATE TABLE $sql_tble(
-                        code VARCHAR(5) NOT NULL,
-                            status VARCHAR(255) NOT NULL,
-                            date DATE NOT NULL,
-                            permission VARCHAR(255),
-                            share VARCHAR(255),
-                            PRIMARY KEY (code)
-                            )";
-
-                    $dropTable = "DROP TABLE $sql_tble";
-                    $insertQuery = "INSERT INTO forumDB (code, status, date, permission, share)
-                    VALUES ('$code' , '$status' , '$date' , '$permission', '$share')";
-
-                    $tableResult = @mysqli_query($conn, $tableExistence);
-
-                    //Checking if the table exist in the database;
-                    if ($tableResult !== FALSE) 
+                    //Storing values
+                    $code = $_POST["statuscode"];
+                    $status = $_POST["status"];
+                    $date = $_POST["date"];
+                    $share = $_POST["share"];
+                    $permisisonInput = $_POST["permission"];
+                    $permission = "";
+                    //Storing permission variables into a single string
+                    foreach ($permisisonInput as $value) 
                     {
-                        insertingQuery($conn, $insertQuery);
-                    } 
-                    else 
-                    {
-                        //Drops the table for insurance
-                        $dropTableResult = @mysqli_query($conn, $dropTable);
-                        $creatingTableResult = @mysqli_query($conn, $creatingTable);
-                        insertingQuery($conn, $insertQuery);
+                        $permission .= $value . " ";
                     }
-                }
-            } 
-        }
 
-        //Checks if the code exist in the database
-        function checkDatabase($checkCodeQuery, $conn)
-        {
-            echo $checkCodeQuery;
-            $checkCodeResult = @mysqli_query($conn, $checkCodeQuery);
-            if(mysqli_num_rows($checkCodeResult) > 0)
-            {
-                echo "<p>The code already exist in the database</p>";
-                return false;
-            }
-            else{
-                return true;
-            }
-        }
+                    $checkCodeQuery = "SELECT * FROM $sql_tble WHERE code = '$code'";
 
-        //Inserting Query into the database
-        function insertingQuery($conn, $insertQuery)
-        {
-            $insertingResult = @mysqli_query($conn, $insertQuery);
-            if ($insertingResult !== FALSE) 
-            {
-                echo "<p>Successful</p>";
-            } 
-            else 
-            {
-                echo "<p>Error inserting into table</p>";
-            }
-        }
-
-        //functions to check if the code is correct
-        function checkCode($code)
-        {
-            //Query to check if the code exist in the database
-            //check if the code is null or empty
-            if (empty($code) || !isset($code)) 
-            {
-                echo "<p>Codebox is empty</p>";
-                return false;
-            }
-             else 
-             {
-                $counter = 0;
-                $codeLen = strlen($code);
-                if ($codeLen == 5) 
-                {
-                    for ($i = 0; $i < $codeLen; $i++) 
+                    //Checking if the code exist in the database 
+                    if(checkDatabase($checkCodeQuery, $conn))
                     {
-                        //Check if the first character is a letter (might need to change it to caps only and give a warning)
-                        if ($i == 0 && !(is_numeric($code[$i])) && $code[$i] == 'S') 
+                        //Sqli queries
+                        $tableExistence = "SELECT 1 FROM $sql_tble";
+                        $creatingTable = "CREATE TABLE $sql_tble(
+                            code VARCHAR(5) NOT NULL,
+                                status VARCHAR(255) NOT NULL,
+                                date DATE NOT NULL,
+                                permission VARCHAR(255),
+                                share VARCHAR(255),
+                                PRIMARY KEY (code)
+                                )";
+
+                        $dropTable = "DROP TABLE $sql_tble";
+                        $insertQuery = "INSERT INTO forumDB (code, status, date, permission, share)
+                        VALUES ('$code' , '$status' , '$date' , '$permission', '$share')";
+
+                        $tableResult = @mysqli_query($conn, $tableExistence);
+
+                        //Checking if the table exist in the database;
+                        if ($tableResult !== FALSE) 
                         {
-                            $counter++;
-                        }
-                        //Check if the remaining characters are numbers only
-                        else if ($i != 0 && is_numeric($code[$i])) 
+                            insertingQuery($conn, $insertQuery);
+                        } 
+                        else 
                         {
-                            $counter++;
+                            //Drops the table for insurance
+                            $dropTableResult = @mysqli_query($conn, $dropTable);
+                            $creatingTableResult = @mysqli_query($conn, $creatingTable);
+                            insertingQuery($conn, $insertQuery);
                         }
                     }
-                    //Checking if the code formmatt is correct (Need something to check if the code exist e.g. use database)
-                    if ($counter == 5) 
-                    {
-                        return true;
-                    } 
-                    else 
-                    {
-                        echo "<p>Please check if you have entered the code in the correct format</p>";
-                        return false;
-                    }
+                } 
+            }
+
+            //Checks if the code exist in the database
+            function checkDatabase($checkCodeQuery, $conn)
+            {
+                echo $checkCodeQuery;
+                $checkCodeResult = @mysqli_query($conn, $checkCodeQuery);
+                if(mysqli_num_rows($checkCodeResult) > 0)
+                {
+                    echo "<p>The code already exist in the database</p>";
+                    return false;
+                }
+                else{
+                    return true;
+                }
+            }
+
+            //Inserting Query into the database
+            function insertingQuery($conn, $insertQuery)
+            {
+                $insertingResult = @mysqli_query($conn, $insertQuery);
+                if ($insertingResult !== FALSE) 
+                {
+                    echo "<p>Successful</p>";
                 } 
                 else 
                 {
-                    echo "<p>Please enter a code of length 5";
+                    echo "<p>Error inserting into table</p>";
+                }
+            }
+
+            //functions to check if the code is correct
+            function checkCode($code)
+            {
+                //Query to check if the code exist in the database
+                //check if the code is null or empty
+                if (empty($code) || !isset($code)) 
+                {
+                    echo "<p>Codebox is empty</p>";
                     return false;
                 }
-            }
-        }
-
-        function checkStatus($status)
-        {
-            if (empty($status) || !isset($status)) 
-            {
-                echo "<p>Status box is empty</p>";
-                return false;
-            } 
-            else 
-            {
-                $pattern = "/[a-zA-Z0-9\s,\.!\?]*$/";
-                //Checks if the status contains only alphabet and spaces
-                if (preg_match($pattern, $status)) 
+                else 
                 {
-                    return true;
+                    $counter = 0;
+                    $codeLen = strlen($code);
+                    if ($codeLen == 5) 
+                    {
+                        for ($i = 0; $i < $codeLen; $i++) 
+                        {
+                            //Check if the first character is a letter (might need to change it to caps only and give a warning)
+                            if ($i == 0 && !(is_numeric($code[$i])) && $code[$i] == 'S') 
+                            {
+                                $counter++;
+                            }
+                            //Check if the remaining characters are numbers only
+                            else if ($i != 0 && is_numeric($code[$i])) 
+                            {
+                                $counter++;
+                            }
+                        }
+                        //Checking if the code formmatt is correct (Need something to check if the code exist e.g. use database)
+                        if ($counter == 5) 
+                        {
+                            return true;
+                        } 
+                        else 
+                        {
+                            echo "<p>Please check if you have entered the code in the correct format</p>";
+                            return false;
+                        }
+                    } 
+                    else 
+                    {
+                        echo "<p>Please enter a code of length 5";
+                        return false;
+                    }
                 }
-                else
+            }
+
+            function checkStatus($status)
+            {
+                if (empty($status) || !isset($status)) 
                 {
-                    echo "<p>Please check if you have any other special characters</p>";
+                    echo "<p>Status box is empty</p>";
+                    return false;
+                } 
+                else 
+                {
+                    $pattern = "/[a-zA-Z0-9\s,\.!\?]*$/";
+                    //Checks if the status contains only alphabet and spaces
+                    if (preg_match($pattern, $status)) 
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        echo "<p>Please check if you have any other special characters</p>";
+                        return false;
+                    }
+                }
+            }
+
+            function validDate($date)
+            {
+                if (empty($date) || !isset($date)) 
+                {
+                    echo "<p>Date box is empty</p>";
                     return false;
                 }
+                return true;
             }
-        }
 
-        function validDate($date)
-        {
-            if (empty($date) || !isset($date)) 
-            {
-                echo "<p>Date box is empty</p>";
-                return false;
-            }
-            return true;
-        }
-
-        //Closing the database
-        mysql_close($conn);
-        ?>
+            //Closing the database
+            mysql_close($conn);
+            ?>
+        </div>
     </div>
 </body>
 
--- searchstatusprocess.php
+++ searchstatusprocess.php
@@ -5,73 +5,75 @@
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
-        <div class = "bodyContainer">
-            <?php
-            require_once('./conf/sqlinfo.php');
+        <div class = "container">
+            <div class = "heading">
+                <h1>Status Posting System</h1>
+            </div>
+            <div class = "bodyContainer">
+                <?php
+                require_once('./conf/sqlinfo.php');
 
-            //Checks if the sqli_connection is a true or false
-            $conn = @mysqli_connect($sql_host,
-            $sql_user,
-            $sql_pass,
-            $sql_db);       
+                //Checks if the sqli_connection is a true or false
+                $conn = @mysqli_connect($sql_host,
+                $sql_user,
+                $sql_pass,
+                $sql_db);       
 
-            if(!$conn)
-            {
-                echo "<p>Database connection failure <p>";
-            }
-            else
-            {
-                echo "<p>Databse connaction successful</p>";
-                //Check if the search input is empty
-                if(!empty($_GET["searchinput"]))
+                if(!$conn)
                 {
-                    echo "<p>input is not blank</p>";
-                    $statusinput = $_GET["searchinput"];
-                    $searchQuery = "SELECT * FROM $sql_tble  WHERE status LIKE '%$statusinput%'";
-                    echo $searchQuery;
-                    $result = @mysqli_query($conn, $searchQuery);
-                    $row = mysqli_fetch_row($result);
-
-                    //starting a table to display the sql queries
-                    echo "<table>";
-
-                    //If the result does not exist
-                    if(!$result)
+                    echo "<p>Database connection failure <p>";
+                }
+                else
+                {
+                    //Check if the search input is empty
+                    if(!empty($_GET["searchinput"]))
                     {
-                        echo "<p>Not found<p>";
+                        $statusinput = $_GET["searchinput"];
+                        $searchQuery = "SELECT * FROM $sql_tble  WHERE status LIKE '%$statusinput%'";
+                        $result = @mysqli_query($conn, $searchQuery);
+                        $row = mysqli_fetch_row($result);
+
+                        //starting a table to display the sql queries
+                        echo "<table>";
+
+                        //If the result does not exist
+                        if(!$result)
+                        {
+                            echo "<p>Not found<p>";
+                            displayingHref();
+                        }
+                        else
+                        {
+                            while($row = mysqli_fetch_assoc($result))
+                            {
+                                echo "<tr><th>Code: </th> <td>",$row["code"],"</td></tr>";
+                                echo "<tr><th>Status: </th><td>",$row["status"],"</td></tr>";
+                                echo "<tr><th>Date: </th><td>",$row["date"],"</td></tr>";
+                                echo "<tr><th>Permission: </th><td>",$row["permission"],"</td></tr>";
+                                echo "<tr><th>Share: </th><td>",$row["share"],"</td></tr>";
+                            }
+                        }  
+                        echo "</table>";
+                        echo "<br>";
+                        mysqli_free_result($result);
                         displayingHref();
                     }
-                    else
-                    {
-                        echo "<p>Chupapi</p>";
-                        while($row = mysqli_fetch_assoc($result))
-                        {
-                            echo "<tr><th>Code: </th> <td>",$row["code"],"</td></tr>";
-                            echo "<tr><th>Status: </th><td>",$row["status"],"</td></tr>";
-                            echo "<tr><th>Date: </th><td>",$row["date"],"</td></tr>";
-                            echo "<tr><th>Permission: </th><td>",$row["permission"],"</td></tr>";
-                            echo "<tr><th>Share: </th><td>",$row["share"],"</td></tr>";
-                        }
-                    }  
-                    echo "</table>";
-                    mysqli_free_result($result);
-                    displayingHref();
+                    else{
+                        echo "<p>Blank</p>";
+                    }
                 }
-                else{
-                    echo "<p>Blank</p>";
+
+                //Closing the database
+                mysql_close($conn);
+
+                //Displaying the links
+                function displayingHref()
+                {
+                    echo '<p><a id = "searchhref" href="http://cyz8072.cmslamp14.aut.ac.nz/assign1/searchstatusform.html">Search status</a>
+                    <a id = "homehref" href="http://cyz8072.cmslamp14.aut.ac.nz/assign1/index.html">Return to Home</a></p>';
                 }
-            }
-
-            //Closing the database
-            mysql_close($conn);
-
-            //Displaying the links
-            function displayingHref()
-            {
-                echo '<p><a id = "searchhref" href="http://cyz8072.cmslamp14.aut.ac.nz/assign1/searchstatusform.html">Search status</a>
-                <a id = "homehref" href="http://cyz8072.cmslamp14.aut.ac.nz/assign1/index.html">Return to Home</a></p>';
-            }
-            ?>
+                ?>
+            </div>
         </div>
     </body>
 </html>
